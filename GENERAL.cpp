e584c5ebcbb70e622f4505f9d2c94eb9cd576fae
MOHAMMED ADEL <sourceyea@gmail.com>
2020-08-01 21:30:04 +0800

    Delete GENERAL.cpp

--- GENERAL.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-cout << "This is just to let you know what programming language was used to create Chains !" << endl;
-cout << "Chains Tool Was Programmed By C++ " << endl;
-
-return 0;
